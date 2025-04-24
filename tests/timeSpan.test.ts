cf339835adf6c70fa58837ad4c3d07f520a206ef
Tokeiya <net_seed@hotmail.com>
2025-04-24 18:29:12 +0900

    Refactor TimeSpan API and update related tests
    Replaced method-based property access with getters for `TimeSpan`, improving API consistency. Updated corresponding test cases to reflect these changes and added constant factors for time conversions. Minor adjustments were made to IntelliJ configuration files.

--- tests/timeSpan.test.ts
+++ tests/timeSpan.test.ts
@@ -4,14 +4,14 @@ import {TimeSpan} from '../ts_src/chrono/timeSpan.js';
 
 test('TimeSpan constructor', () => {
 	const timeSpan = new TimeSpan(123456789n);
-	expect(timeSpan.elapsed).toBe(123456789n);
+	expect(timeSpan.picoSeconds).toBe(123456789n);
 });
 
 test('TimeSpan create', () => {
 	const start = ChronoAnchor.fromHrtime(123456789n, 1);
 	const end = ChronoAnchor.fromHrtime(987654321n, 1);
 	const timeSpan = TimeSpan.create(start, end);
-	expect(timeSpan.elapsed).toBe(864197532n);
+	expect(timeSpan.picoSeconds).toBe(864197532n);
 });
 
 test('TimeSpan create invalid identity', () => {
@@ -22,59 +22,59 @@ test('TimeSpan create invalid identity', () => {
 
 test('TimeSpan totalPicoseconds', () => {
 	const timeSpan = new TimeSpan(123456789n);
-	expect(timeSpan.totalPicoseconds()).toBe(123456789n);
+	expect(timeSpan.totalPicoSeconds).toBe(123456789n);
 });
 
 test('TimeSpan totalNanoseconds', () => {
 	const timeSpan = new TimeSpan(1234567890n);
-	const result = timeSpan.totalNanoseconds();
+	const result = timeSpan.totalNanoseconds;
 	console.log(result);
 	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalMicroseconds', () => {
 	const timeSpan = new TimeSpan(1234_567_890n);
-	const result = timeSpan.totalMicroseconds();
+	const result = timeSpan.totalMicroseconds;
 	console.log(result);
 	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalMilliseconds', () => {
 	const timeSpan = new TimeSpan(123_456_789_012n);
-	const result = timeSpan.totalMilliseconds();
+	const result = timeSpan.totalMilliseconds;
 	console.log(result);
 	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalSeconds', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345n);
-	const result = timeSpan.totalSeconds();
+	const result = timeSpan.totalSeconds;
 	console.log(result);
 	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalMinutes', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345_678n);
-	const result = timeSpan.totalMinutes();
+	const result = timeSpan.totalMinutes;
 	console.log(result);
 	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalHours', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345_678_901n);
-	const result = timeSpan.totalHours();
+	const result = timeSpan.totalHours;
 	console.log(result);
 	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalDays', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345_678_901_234n);
-	const result = timeSpan.totalDays();
+	const result = timeSpan.totalDays;
 	console.log(result);
 	throw new Error('Not implemented')
 });
 
 test('picoseconds', () => {
 	const timeSpan = new TimeSpan(123456789n);
-	expect(timeSpan.picoSeconds()).toBe(0n);
+	expect(timeSpan.picoSeconds).toBe(0n);
 });
--- ts_src/chrono/timeSpan.ts
+++ ts_src/chrono/timeSpan.ts
@@ -1,8 +1,16 @@
 import { ChronoAnchor } from './chronoAnchor';
 import { DoubleDouble } from '../dd/doubleDouble';
 
+const NANO_FACTOR=DoubleDouble.fromBigint(1_000n);
+const MICRO_FACTOR=DoubleDouble.fromBigint(1_000_000n);
+const MILLI_FACTOR=DoubleDouble.fromBigint(1_000_000_000n);
+const SECOND_FACTOR=DoubleDouble.fromBigint(1_000_000_000_000n);
+const MINUTE_FACTOR=SECOND_FACTOR.mul(DoubleDouble.fromBigint(60n))
+const HOUR_FACTOR=MINUTE_FACTOR.mul(DoubleDouble.fromBigint(60n));
+
+
 export class TimeSpan{
-	public readonly elapsed:bigint
+	public readonly totalPicoSeconds:bigint
 
 	constructor(elapsed:bigint){
 		throw new Error('Not implemented')
@@ -12,63 +20,60 @@ export class TimeSpan{
 		throw new Error('Not implemented')
 	}
 
-	public totalPicoseconds():bigint{
+
+	public get totalNanoseconds():DoubleDouble{
 		throw new Error('Not implemented')
 	}
 
-	public totalNanoseconds():DoubleDouble{
+	public get totalMicroseconds():DoubleDouble{
 		throw new Error('Not implemented')
 	}
 
-	public totalMicroseconds():DoubleDouble{
+	public get totalMilliseconds():DoubleDouble{
 		throw new Error('Not implemented')
 	}
 
-	public totalMilliseconds():DoubleDouble{
+	public get totalSeconds():number{
 		throw new Error('Not implemented')
 	}
 
-	public totalSeconds():number{
+	public get totalMinutes():number{
 		throw new Error('Not implemented')
 	}
 
-	public totalMinutes():number{
+	public get totalHours():number{
 		throw new Error('Not implemented')
 	}
 
-	public totalHours():number{
+	public get totalDays():number{
 		throw new Error('Not implemented')
 	}
 
-	public totalDays():number{
+	public get picoSeconds():number{
 		throw new Error('Not implemented')
 	}
 
-	public picoSeconds():number{
+	public get nanoSeconds():number{
 		throw new Error('Not implemented')
 	}
 
-	public nanoSeconds():number{
+	public get microSeconds():number{
 		throw new Error('Not implemented')
 	}
 
-	public microSeconds():number{
+	public get milliSeconds():number{
 		throw new Error('Not implemented')
 	}
 
-	public milliSeconds():number{
+	public get seconds():number{
 		throw new Error('Not implemented')
 	}
 
-	public seconds():number{
+	public get minutes():number{
 		throw new Error('Not implemented')
 	}
 
-	public minutes():number{
-		throw new Error('Not implemented')
-	}
-
-	public hours():number{
+	public get hours():number{
 		throw new Error('Not implemented')
 	}
 
@@ -85,6 +90,10 @@ export class TimeSpan{
 	}
 
 	public divide(divisor:number):TimeSpan{
+		throw new Error('Not implemented')
+	}
+
+	public divide_ts(timeSpan:TimeSpan):number{
 		throw new Error('Not implemented')
 	}
 
