6a412824eb7ceb32ba8af9046bc65509be3d6eab
Tokeiya <net_seed@hotmail.com>
2025-04-24 17:07:19 +0900

    Update test cases and dependencies, adjust IntelliJ configs
    Replaced `fail()` with `throw new Error('Not implemented')` in unit tests for `TimeSpan`. Updated dependencies in `package.json` with minor version bumps and updated the `pnpm-lock.yaml` accordingly. Modified IntelliJ IDEA settings files to reflect recent changes and configurations.

--- tests/timeSpan.test.ts
+++ tests/timeSpan.test.ts
@@ -1,5 +1,4 @@
-import {test, expect,fail} from 'vitest';
-import {DoubleDouble} from '../ts_src/dd/doubleDouble.js';
+import {test, expect} from 'vitest';
 import {ChronoAnchor} from '../ts_src/chrono/chronoAnchor.js';
 import {TimeSpan} from '../ts_src/chrono/timeSpan.js';
 
@@ -30,49 +29,49 @@ test('TimeSpan totalNanoseconds', () => {
 	const timeSpan = new TimeSpan(1234567890n);
 	const result = timeSpan.totalNanoseconds();
 	console.log(result);
-	fail()
+	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalMicroseconds', () => {
 	const timeSpan = new TimeSpan(1234_567_890n);
 	const result = timeSpan.totalMicroseconds();
 	console.log(result);
-	fail()
+	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalMilliseconds', () => {
 	const timeSpan = new TimeSpan(123_456_789_012n);
 	const result = timeSpan.totalMilliseconds();
 	console.log(result);
-	fail()
+	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalSeconds', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345n);
 	const result = timeSpan.totalSeconds();
 	console.log(result);
-	fail()
+	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalMinutes', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345_678n);
 	const result = timeSpan.totalMinutes();
 	console.log(result);
-	fail()
+	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalHours', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345_678_901n);
 	const result = timeSpan.totalHours();
 	console.log(result);
-	fail()
+	throw new Error('Not implemented')
 });
 
 test('TimeSpan totalDays', () => {
 	const timeSpan = new TimeSpan(123_456_789_012_345_678_901_234n);
 	const result = timeSpan.totalDays();
 	console.log(result);
-	fail()
+	throw new Error('Not implemented')
 });
 
 test('picoseconds', () => {
