09c2eb932fcc30bd44afcbc47c3ed0d69701c4c4
Tokeiya <net_seed@hotmail.com>
2025-04-25 17:11:31 +0900

    Remove identity handling from ChronoAnchor class
    Eliminated the `identity` property and related logic from the `ChronoAnchor` class and test cases. This simplifies the implementation by focusing solely on the `anchor` attribute.

--- tests/chronoAnchor.test.ts
+++ tests/chronoAnchor.test.ts
@@ -4,32 +4,16 @@ import {ChronoAnchor} from '../ts_src/chrono/chronoAnchor.js';
 
 
 test('fromHrtime', () => {
-	const anchor = ChronoAnchor.fromHrtime(123456789n, 1);
+	const anchor = ChronoAnchor.fromHrtime(123456789n);
 	expect(anchor.anchor).toBe(123456789n);
-	expect(anchor.identity).toBe("1");
 })
 
 test('fromPerformance', () => {
 
-	let anchor = ChronoAnchor.fromPerformance(123.456, 1);
+	let anchor = ChronoAnchor.fromPerformance(123.456);
 	expect(anchor.anchor).toBe(123_456_000n);
-	expect(anchor.identity).toBe("1");
 
-	anchor = ChronoAnchor.fromPerformance(123.456789012, undefined);
+	anchor = ChronoAnchor.fromPerformance(123.456789012);
 	expect(anchor.anchor).toBe(123_456_789n);
-	expect(anchor.identity).toBeUndefined();
 })
 
-test('checkIdentity', () => {
-	const anchor1 = ChronoAnchor.fromHrtime(123456789n, 1);
-	const anchor2 = ChronoAnchor.fromHrtime(123456789n, 1);
-	const anchor3 = ChronoAnchor.fromHrtime(123456789n, undefined);
-	const anchor4 = ChronoAnchor.fromHrtime(123456789n, undefined);
-	const anchor5 = ChronoAnchor.fromHrtime(987654321n, 2);
-
-	expect(anchor1.checkIdentity(anchor2)).toBe(true);
-	expect(anchor1.checkIdentity(anchor3)).toBe(false);
-	expect(anchor3.checkIdentity(anchor4)).toBe(true);
-	expect(anchor1.checkIdentity(anchor5)).toBe(false);
-
-})
--- ts_src/chrono/chronoAnchor.ts
+++ ts_src/chrono/chronoAnchor.ts
@@ -1,29 +1,16 @@
 export class ChronoAnchor{
 	public readonly anchor:bigint
-	public readonly identity:string|undefined
 
-	private constructor(anchor:bigint, identity:string|undefined) {
+	private constructor(anchor:bigint) {
 		this.anchor = anchor
-		this.identity = identity
 	}
 
-	public static fromHrtime(value:bigint, identity:number|undefined):ChronoAnchor {
-		if (identity === undefined) {
-			return new ChronoAnchor(value, undefined)
-		}else{
-			return new ChronoAnchor(value, identity.toString())
-		}
+	public static fromHrtime(value:bigint):ChronoAnchor {
+			return new ChronoAnchor(value)
 	}
 
-	public static fromPerformance(value:number, identity:number|undefined):ChronoAnchor {
-		if(identity === undefined) {
-			return new ChronoAnchor(BigInt(Math.round(value * 1_000_000)), undefined)
-		}else{
-			return new ChronoAnchor(BigInt(Math.round(value * 1_000_000)), identity.toString())
-		}
+	public static fromPerformance(value:number):ChronoAnchor {
+			return new ChronoAnchor(BigInt(Math.round(value * 1_000_000)))
 	}
 
-	public checkIdentity(other:ChronoAnchor):boolean {
-		return this.identity===other.identity
-	}
 }
