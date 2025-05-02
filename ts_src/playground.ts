a11f1c9fd66688d68031dc64d3dd05d24533aba5
Tokeiya <net_seed@hotmail.com>
2025-05-02 17:12:28 +0900

    revert and wip add regex cheat sheet.

--- ts_src/playground.ts
+++ ts_src/playground.ts
@@ -1,8 +1,4 @@
 import {DoubleDouble} from './dd/doubleDouble.js';
 
-
-const input="123.456";
-const regex=/\d*/g;
-
-let result=[...input.matchAll(regex)];
-console.log(result);
+const n=42;
+const bi=BigInt(n);
